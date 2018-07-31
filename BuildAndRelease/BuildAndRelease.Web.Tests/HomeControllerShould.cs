58406df3b57a107a298f777a349472eb0d824879
Kasun Kodagoda <KasunK@99x.lk>
2018-07-31 22:12:48 +0530

    Added some unit tests

--- BuildAndRelease/BuildAndRelease.Web.Tests/HomeControllerShould.cs
+++ BuildAndRelease/BuildAndRelease.Web.Tests/HomeControllerShould.cs
@@ -1,4 +1,5 @@
 using BuildAndRelease.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -6,12 +7,40 @@ namespace BuildAndRelease.Web.Tests
 {
     public class HomeControllerShould
     {
+        private readonly HomeController _controller;
+
+        public HomeControllerShould()
+        {
+            _controller = new HomeController();
+        }
+
         [Fact]
         public void ReturnViewWhenIndexIsCalled()
         {
-            var controller = new HomeController();
-            var index = controller.Index();
-            Assert.NotNull(index);
+            var result = _controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void ReturnViewWhenAboutIsCalled()
+        {
+            var about = _controller.About();
+            Assert.IsType<ViewResult>(about);
+        }
+
+        [Fact]
+        public void ReturnViewWhenContractIsCalled()
+        {
+            var contact = _controller.Contact();
+            Assert.IsType<ViewResult>(contact);
+        }
+
+        [Fact]
+        public void ReturnViewWhenPrivacyIsCalled()
+        {
+            var privacy = _controller.Privacy();
+            Assert.IsType<ViewResult>(privacy);
         }
     }
+
 }
